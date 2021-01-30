69d711b287c23d4b7bfed879ac9236c55bbf5f10
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-01-30 17:05:10 -0500

    set defaults for bday hunger happy

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -7,9 +7,9 @@ namespace NEWTamagotchiAPI.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime Birthday { get; set; }
-        public int HungerLevel { get; set; }
-        public int HappinessLevel { get; set; }
+        public DateTime Birthday { get; set; } = DateTime.Now;
+        public int HungerLevel { get; set; } = 0;
+        public int HappinessLevel { get; set; } = 0;
         public List<Playtime> Playtimes { get; set; }
         public List<Scolding> Scoldings { get; set; }
         public List<Feeding> Feedings { get; set; }
